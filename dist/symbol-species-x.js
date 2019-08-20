7545075bcb06671fe742a8ac31b290bc35f49c7a
Graham Fairweather <xotic750@gmail.com>
2019-08-20 10:52:39 +0200

    Maintenance

--- dist/symbol-species-x.js
+++ dist/symbol-species-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-15T17:40:00.870Z",
+  "date": "2019-08-20T08:52:33.756Z",
   "describe": "",
   "description": "System Symbol.species (real or fake)",
   "file": "symbol-species-x.js",
